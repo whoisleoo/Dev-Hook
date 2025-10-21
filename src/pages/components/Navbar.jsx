f7741b329aaefa4d07be28dbb5b321175c5d3da3
whoisleoo <leoaaa96@gmail.com>
2025-10-21 11:43:59 -0300

    Adição da navbar do devhook

---
+++ src/pages/components/Navbar.jsx
@@ -0,0 +1,30 @@
+function NavBar(){
+
+return(
+<div className="min-h-screen bg-black text-white">
+  <nav className="flex items-center justify-between  py-6 max-w-9xl mx-auto">
+    <div className="flex items-center">
+      <img className="w-25 h-25" src="./logo.svg" alt="aqui é uma logo bonita" />
+    </div>
+
+    <div className="flex gap-25 text-md">
+      <a href="#" className="hover:text-purple-400 transition-colors">Home</a>
+      <a href="#" className="hover:text-purple-400 transition-colors">How it Works</a>
+      <a href="#" className="hover:text-purple-400 transition-colors">Support</a>
+    </div>
+
+    <div className="flex gap-4">
+      <button className="px-9 py-2 border border-white rounded-lg hover:bg-white hover:text-black transition-all">
+        Sign Up
+      </button>
+      <button className="px-6 py-2 bg-purple-600 rounded-lg hover:bg-purple-700 transition-all">
+    Log In
+      </button>
+    </div>
+  </nav>
+</div>
+)
+}
+
+
+export default NavBar
